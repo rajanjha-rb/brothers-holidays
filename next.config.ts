cf37bf8c68816321f9093e4bff94ad6672e240f9
Rajan Jha <rajanjharb@gmail.com>
2025-07-18 09:55:59 +0545

    improved performance

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-// Add bundle analyzer
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+export default nextConfig;
